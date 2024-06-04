72f327f543af95bee5c478191696538116a0881f
Gleb Savchenko <savchenko.gleb.97@gmail.com>
2024-06-04 11:06:20 +0200

    bash_plugins: fix pathes in doc

--- bash_plugins/funcs/doc.sh
+++ bash_plugins/funcs/doc.sh
@@ -2,43 +2,19 @@
 doc() {
   case "$1" in
     cpp1)
-      v $HOME/txt/study/courses/cpp1.md
+      vim $HOME/txt/study/courses/cpp1.md
       ;;
     cpp2)
-      v $HOME/txt/study/courses/cpp2.md
-      ;;
-    algo1)
-      v $HOME/txt/study/courses/algo1.md
-      ;;
-    algo2)
-      v $HOME/txt/study/courses/algo2.md
-      ;;
-    concurrency)
-      v $HOME/txt/study/courses/concurrency.md
-      ;;
-    os)
-      v $HOME/txt/study/courses/os.md
-      ;;
-    dist)
-      v $HOME/txt/study/courses/dist.md
+      vim $HOME/txt/study/courses/cpp2.md
       ;;
     go)
-      v $HOME/txt/study/courses/go.md
+      vim $HOME/txt/study/courses/go.md
       ;;
     rust)
-      v $HOME/txt/study/courses/rust.md
+      vim $HOME/txt/study/courses/rust.md
       ;;
     py)
-      v $HOME/txt/study/courses/python.md
-      ;;
-    books)
-      v $HOME/txt/study/books.md
-      ;;
-    health)
-      v -O $HOME/txt/health/diary.md $HOME/txt/health/bp.md
-      ;;
-    mental)
-      v $HOME/txt/health/mental.md
+      vim $HOME/txt/study/courses/python.md
       ;;
     *)
       echo "doc: unknown key '$1'"
@@ -46,4 +22,4 @@ doc() {
       ;;
   esac
 }
-complete -W "health mental books algo1 algo2 cpp1 cpp2 concurrency os dist rust go py" doc
+complete -W "cpp1 cpp2 rust go py" doc
