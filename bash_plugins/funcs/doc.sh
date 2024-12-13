0b7be4fe3f29748d35982b6604fb6d3370cb1048
Gleb Savchenko <savchenko.gleb.97@gmail.com>
2024-12-13 13:00:40 +0100

    bash_plugins: add more courses to doc

--- bash_plugins/funcs/doc.sh
+++ bash_plugins/funcs/doc.sh
@@ -16,10 +16,22 @@ doc() {
     py)
       vim $HOME/txt/study/courses/python.md
       ;;
+    os)
+      vim $HOME/txt/study/courses/os.md
+      ;;
+    db1)
+      vim $HOME/txt/study/courses/db1.md
+      ;;
+    db2)
+      vim $HOME/txt/study/courses/db2.md
+      ;;
+    db)
+      vim $HOME/txt/study/courses/db.md
+      ;;
     *)
       echo "doc: unknown key '$1'"
       return 1
       ;;
   esac
 }
-complete -W "cpp1 cpp2 rust go py" doc
+complete -W "cpp1 cpp2 rust go py os db1 db2 db" doc
