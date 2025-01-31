d33f1de208c291c4c8f5fe07e65c65f68df3c87f
Gleb Savchenko <savchenko.gleb.97@gmail.com>
2025-01-31 19:51:21 +0100

    bash_plugins: turn on fzf's dracula theme

--- bash_plugins/exports.sh
+++ bash_plugins/exports.sh
@@ -15,3 +15,6 @@ export TIMEFMT=$'\nreal\t%E\nuser\t%U\nsys\t%S'
 
 ## setting for tmuxp
 export DISABLE_AUTO_TITLE='true'
+
+## Dracula theme for FZF
+export FZF_DEFAULT_OPTS='--color=fg+:#f8f8f2,bg+:#44475a,hl+:#bd93f9 --color=info:#ffb86c,prompt:#50fa7b,pointer:#ff79c6 --color=marker:#ff79c6,spinner:#ffb86c,header:#6272a4'
--- bash_plugins/finalize.sh
+++ bash_plugins/finalize.sh
@@ -7,7 +7,6 @@
 # eval "$(pyenv virtualenv-init -)"
 
 ## setup for fzf utility
-# export FZF_DEFAULT_OPTS='--color=fg+:#f8f8f2,bg+:#44475a,hl+:#bd93f9 --color=info:#ffb86c,prompt:#50fa7b,pointer:#ff79c6 --color=marker:#ff79c6,spinner:#ffb86c,header:#6272a4'
 [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
 
 # To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
